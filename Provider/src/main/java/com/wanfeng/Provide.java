e35c9834c45091dfcb9e457c00a1dc20e95f6522
晚风 <2580330397@qq.com>
2023-04-11 15:43:24 +0800

    2023-04-11 commit

--- Provider/src/main/java/com/wanfeng/Provide.java
+++ Provider/src/main/java/com/wanfeng/Provide.java
@@ -1,7 +1,9 @@
 package com.wanfeng;
 
+import com.wanfeng.common.URL;
 import com.wanfeng.protocol.HttpServer;
 import com.wanfeng.register.LocalRegister;
+import com.wanfeng.register.RemoteRegister;
 
 /**
  * Date: 2023-04-10 0010 18:01
@@ -10,13 +12,16 @@ import com.wanfeng.register.LocalRegister;
  */
 public class Provide {
 	public static void main(String[] args) {
-		//注册（存放）接口名和实现类的映射
+		//本地注册（存放）接口名和实现类的映射
 		LocalRegister.register(HelloService.class.getName(),"1.0", HelloServiceImpl.class);
 		LocalRegister.register(HelloService.class.getName(),"2.0", HelloServiceImpl2.class);
 
+		//注册中心注册
+		URL url = new URL("localhost", 8080);
+		RemoteRegister .register(HelloService.class.getName(), "1.0", url);
 
 		//开启Http服务
 		HttpServer httpServer = new HttpServer();
-		httpServer.start("localhost", 8080);
+		httpServer.start(url.getHostname(), url.getPort());
 	}
 }
